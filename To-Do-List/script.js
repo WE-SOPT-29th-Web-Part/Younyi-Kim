2521dba8a77708ee1e1661acf5df7eb526d18848
younyikim <rlazmzm1123@gmail.com>
2021-10-18 14:00:00 +0900

    fetch : todo list add btn/delete btn/nav btn 필터링구현

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,57 +1,18 @@
 "use strict";
 
-// 1. add 버튼을 눌렀을 때, list가 추가되도록
-const addBtn = document.querySelector(".todos__add");
+// 구현 기능
+// 1. 추가 버튼(+) 클릭 시, 아이템 추가
+// 2. Enter시 아이템 추가
+// 3. 삭제 버튼(x) 클릭 시, 아이템 삭제
+// 4. 오늘 할일, 내일 할일, 모두 보기 버튼
+
+const addBtns = document.querySelectorAll(".todos__add");
 const deleteBtn = document.querySelector(".todos__delete");
-const input = document.querySelector(".todos__input");
-const todosItems = document.querySelector(".todos__items");
-
-function addTodoItem(index) {
-    const item = document.createElement("li");
-    item.classList.add("todos__item");
-
-    item.innerHTML = `
-        <div class="todos__name">${input.value}</div>
-        <button class="todos__delete">
-            <i class="fas fa-times"></i>
-        </button>
-    `;
-    input.value = "";
-    todosItems.appendChild(item);
-
-    // deleteBtn.addEventListener("click", () => {
-    //     console.log("remove");
-    //     item.removeChild();
-    // });
-}
-
-addBtn.addEventListener("click", () => {
-    addTodoItem();
-});
-
-// addBtns.forEach((btn, index) => {
-//     btn.addEventListener("click", () => {
-//         addTodoItem(index);
-//     });
-// });
-
-// 2. Enter를 눌렀을 때도, item을 추가한다.
-// inputs.forEach((input, index) => {
-//     input.addEventListener("click", () => {
-//         addTodoItem(index);
-//     });
-// });
-input.addEventListener("keyup", (event) => {
-    if (input.value == "") return;
-    if (event.key == "Enter") {
-        addTodoItem();
-    }
-});
-
+const inputs = document.querySelectorAll(".todos__input");
+const todosItems = document.querySelectorAll(".todos__items");
 const nav = document.querySelector(".options");
 const todos = document.querySelectorAll(".todos > section");
 
-console.log(todos);
 nav.addEventListener("click", (event) => {
     if (event.target.className.includes("options__today")) {
         todos[0].classList.add("open");
@@ -64,3 +25,56 @@ nav.addEventListener("click", (event) => {
         todos[1].classList.add("open");
     }
 });
+
+// 추가 버튼 클릭 시 handleInput 함수 실행
+addBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        handleInput(index);
+    });
+});
+
+// input value 입력 후, enter키 눌렀을 시, handleInput 함수 실행
+inputs.forEach((input, index) => {
+    input.addEventListener("keyup", (event) => {
+        if (!input.value) return;
+        if (event.key == "Enter") {
+            handleInput(index);
+        }
+    });
+});
+
+// 새로운 to-do 목록을 만드는 함수이다.
+function createItem(text, index) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const icon = document.createElement("i");
+
+    span.innerText = text;
+
+    icon.setAttribute("class", "fas fa-times todos__delete");
+    li.setAttribute("class", "todos__item");
+
+    li.appendChild(span);
+    li.appendChild(icon);
+
+    icon.addEventListener("click", () => {
+        deleteItem(li, index);
+    });
+
+    todosItems[index].appendChild(li);
+}
+
+// to-do 목록을 삭제하는 함수이다.
+function deleteItem(li, index) {
+    todosItems[index].removeChild(li);
+}
+
+// input을 제어하고, createItem을 호출하는 함수이다.
+function handleInput(index) {
+    const text = inputs[index].value;
+    inputs[index].value = "";
+    inputs[index].focus();
+    if (!text) return;
+
+    createItem(text, index);
+}
