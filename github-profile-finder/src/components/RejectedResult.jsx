1d7fd4a9d319cd67edf818b3ab990c69a6b73a44
younyikim <rlazmzm1123@gmail.com>
2021-11-02 17:38:11 +0900

    feat:4차 세미나 과제 pending page animation 구현

--- github-profile-finder/src/components/RejectedResult.jsx
+++ github-profile-finder/src/components/RejectedResult.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 const RejectedResult = () => {
-    return <Root>사용자가 없습니다</Root>;
+    return <Root>사용자가 없습니다.</Root>;
 };
 
 export default RejectedResult;
 
 const Root = styled.div`
+    margin-top: 20px;
     font-size: 32px;
-    color: white;
+    color: whitesmoke;
 `;
