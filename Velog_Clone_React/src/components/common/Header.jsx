295e631faf13dbb2b34d1dcb3c1221394e2f885b
younyikim <rlakuku1221@gmail.com>
2021-11-12 13:55:34 +0900

    feat:svg fill(velog logo & drop down arrow

--- Velog_Clone_React/src/components/common/Header.jsx
+++ Velog_Clone_React/src/components/common/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { ReactComponent as ArrowDropDownImage } from "../../assets/icons/arrow-down.svg";
+import { ReactComponent as VelogIcon } from "../../assets/icons/velog-icon.svg";
 import { FaCaretDown } from "react-icons/fa";
 
 const Header = () => {
     return (
         <StyledHeader>
             <StyledLeft>
-                <svg width="22" height="22" viewBox="0 0 192 192" fill="none">
+                {/* <svg width="22" height="22" viewBox="0 0 192 192" fill="none">
                     <rect
                         width="192"
                         height="192"
@@ -18,7 +20,8 @@ const Header = () => {
                         d="M49 65.48V57.92C53.8 56.36 59.44 54.68 65.92 52.88C72.4 50.96 76.78 50 79.06 50C84.1 50 87.1 52.4 88.06 57.2L99.76 123.62C103.48 118.7 106.54 114.56 108.94 111.2C112.66 105.92 116.08 99.86 119.2 93.02C122.44 86.18 124.06 80.06 124.06 74.66C124.06 71.42 123.16 68.84 121.36 66.92C119.68 64.88 116.5 62.3 111.82 59.18C116.62 53.06 122.62 50 129.82 50C133.66 50 136.84 51.14 139.36 53.42C142 55.7 143.32 59.06 143.32 63.5C143.32 70.94 140.2 80.24 133.96 91.4C127.84 102.44 116.02 119.06 98.5 141.26L80.68 142.52L67 65.48H49Z"
                         fill="white"
                     ></path>
-                </svg>
+                </svg> */}
+                <VelogIcon />
                 <a>gokuku.log</a>
             </StyledLeft>
             <StyledRight>
@@ -33,7 +36,8 @@ const Header = () => {
                     />
                 </div>
                 <button>
-                    <FaCaretDown />
+                    {/* <FaCaretDown /> */}
+                    <ArrowDropDownImage fill="black" />
                 </button>
             </StyledRight>
         </StyledHeader>
@@ -64,6 +68,10 @@ const StyledLeft = styled.div`
         font-weight: bold;
         margin-left: 10px;
     }
+
+    &:hover svg {
+        fill: rgb(32, 201, 151);
+    }
 `;
 
 const StyledRight = styled.div`
@@ -79,6 +87,9 @@ const StyledRight = styled.div`
         background-color: inherit;
     }
 
+    &:hover svg {
+        fill: rgb(32, 201, 151);
+    }
     a button {
         height: 30px;
         padding-left: 15px;
